8843074f14d0539efd8028903950ba1deb24fce8
Casey <clknox23@gmail.com>
2025-08-21 17:36:55 -0400

    adversarial

--- A4/network_visualization.py
+++ A4/network_visualization.py
@@ -94,8 +94,8 @@ def make_adversarial_attack(X, target_y, model, max_iter=100, verbose=True):
   ##############################################################################
   for i in range(max_iter):
     y_pred = model.forward(X_adv)
-    max_score = torch.max(y_pred).values
-    target_score = y_pred[:, target_y]
+    max_score = torch.max(y_pred).values.item()
+    target_score = y_pred[:, target_y].item()
     if verbose:
       print(f"Iter {i:04}/{max_iter} | Target Score {target_score:.3f} | Max Score {max_score:.3f}")
 
@@ -103,7 +103,7 @@ def make_adversarial_attack(X, target_y, model, max_iter=100, verbose=True):
       break
 
     y = torch.zeros_like(y_pred)
-    y[:, torch.argmax(y_pred)] = 1
+    y[:, target_y] = 1
     loss = F.cross_entropy(y_pred, y)
     loss.backward()
 
