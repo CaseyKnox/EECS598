a5b486e4023d6b6b5ed257f15e106e610a2d7216
Casey <clknox23@gmail.com>
2025-09-03 20:07:14 -0400

    sim vectorized

--- assignment3/cs231n/simclr/contrastive_loss.py
+++ assignment3/cs231n/simclr/contrastive_loss.py
@@ -100,9 +100,13 @@ def sim_positive_pairs(out_left, out_right):
     ##############################################################################
     
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-    l = out_left  / torch.linalg.norm(out_left,  dim=1, keepdim=True)
-    r = out_right / torch.linalg.norm(out_right, dim=1, keepdim=True)
-    pos_pairs = (l * r).sum()
+
+    norm_l = torch.linalg.norm(out_left,  dim=1)       # (N,)
+    norm_r = torch.linalg.norm(out_right, dim=1)       # (N,)
+    norm = norm_l * norm_r
+
+    pos_pairs = torch.sum(out_left * out_right, dim=1) # (N, 1)
+    pos_pairs /= norm
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     
